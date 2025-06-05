1882f864f50188499ab41468d85d1cd18a68e4b9
serendipity019 <navagio09@gmail.com>
2025-06-05 23:17:15 +0300

    feat: we make UserEntity and Role Enum

---
+++ src/main/java/gr/aueb/cf/teacherapp/core/enums/Role.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.teacherapp.core.enums;
+
+public enum Role {
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
---
+++ src/main/java/gr/aueb/cf/teacherapp/model/UserEntity.java
@@ -0,0 +1,64 @@
+package gr.aueb.cf.teacherapp.model;
+
+import gr.aueb.cf.teacherapp.core.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name= "users")
+public class UserEntity extends AbstractEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false )
+    private Long id;
+
+    @Column(nullable = false )
+    private String username;
+
+    @Column(nullable = false )
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+}
